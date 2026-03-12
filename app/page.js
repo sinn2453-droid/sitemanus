95b982e76fda0ce3af23250987125781fb378d69
sinn2453-droid <sinn2453@gmail.com>
2026-03-13 00:11:19 +0200

    Revamp Home component layout with new sections

--- app/page.js
+++ app/page.js
@@ -1,9 +1,28 @@
-
 export default function Home() {
   return (
-    <main>
-      <h1>Привіт як справи</h1>
-      <p>Мій перший сайт на Next.js</p>
-    </main>
-  )
+    <div>
+
+      <div className="header">
+        <div className="avatar"></div>
+        <h2>Мій профіль</h2>
+      </div>
+
+      <div className="card">
+        <h3>Публікацій ще немає</h3>
+        <p>Опублікуйте фото або відео</p>
+      </div>
+
+      <div style={{textAlign:"center"}}>
+        <button className="button">Додати публікацію</button>
+      </div>
+
+      <div className="navbar">
+        <div>Чати</div>
+        <div>Контакти</div>
+        <div>Налаштування</div>
+        <div>Профіль</div>
+      </div>
+
+    </div>
+  );
 }
