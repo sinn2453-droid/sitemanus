fcc2dc9501570d28617ad6318e61032937017bed
sinn2453-droid <sinn2453@gmail.com>
2026-03-12 23:32:01 +0200

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding: "40px", fontFamily: "Arial"}}>
-      <h1>Привіт як справи</h1>
-      <p>Мій перший сайт на Next.js</p>
-    </main>
-  )
-}
