423710cc3e4e029326ad58637daa1752f0c3fbc7
sinn2453-droid <sinn2453@gmail.com>
2026-03-13 00:01:48 +0200

    Refactor layout.js for improved body formatting

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,9 @@
-export const metadata = {
-  title: "My Site",
-  description: "My first Next.js site"
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
