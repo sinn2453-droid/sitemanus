b97dd7d5786d4d81a604607252a87ca562f3f0a0
sinn2453-droid <sinn2453@gmail.com>
2026-03-14 00:25:04 +0200

    Clean up RootLayout component
    Removed metadata and comments from RootLayout.

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,13 @@
 import "./globals.css";
 import Sidebar from "../components/Sidebar";
 
-export const metadata = {
-  title: "BUTBIT",
-  description: "Соціальна мережа BUTBIT",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
       <body>
 
-        {/* Google icons */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded"
-          rel="stylesheet"
-        />
-
-        {/* бокова панель */}
         <Sidebar />
 
-        {/* сторінки */}
         {children}
 
       </body>
