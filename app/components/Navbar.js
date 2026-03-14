f97748339862225875341dbf39d0b5c2cfa13fe6
sinn2453-droid <sinn2453@gmail.com>
2026-03-14 19:35:45 +0200

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,1 +1,33 @@
+"use client";
 
+export default function Navbar(){
+  return(
+
+    <div className="navbar">
+
+      <span className="material-symbols-rounded">
+        home
+      </span>
+
+      <span className="material-symbols-rounded">
+        search
+      </span>
+
+      <div className="addButton">
+        <span className="material-symbols-rounded">
+          add
+        </span>
+      </div>
+
+      <span className="material-symbols-rounded">
+        chat
+      </span>
+
+      <span className="material-symbols-rounded">
+        person
+      </span>
+
+    </div>
+
+  );
+}
