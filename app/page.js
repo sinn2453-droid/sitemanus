7070ce6bbee7931c77467605781f99752bdabf68
sinn2453-droid <sinn2453@gmail.com>
2026-03-14 19:51:36 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,2 +1,42 @@
+"use client";
 
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
+import { useRouter } from "next/navigation";
+
+export default function Home(){
+
+  const router = useRouter();
+
+  useEffect(()=>{
+
+    supabase.auth.getUser().then(({ data })=>{
+
+      if(!data.user){
+        router.push("/login");
+      }
+
+    });
+
+  },[]);
+
+  return(
+
+    <div className="app">
+
+      <div className="glass profileCard">
+
+        <div className="avatar"></div>
+
+        <h2>BUTBIT</h2>
+
+        <p>BUTBIT</p>
+
+      </div>
+
+    </div>
+
+  );
+
+}
 
