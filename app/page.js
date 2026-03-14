37d59a02455bf7c9c5fcae6a141ad784dafad352
sinn2453-droid <sinn2453@gmail.com>
2026-03-14 19:54:55 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 import { useRouter } from "next/navigation";
 
@@ -8,17 +8,54 @@ export default function Home(){
 
   const router = useRouter();
 
+  const [loading,setLoading] = useState(true);
+  const [user,setUser] = useState(null);
+
   useEffect(()=>{
 
-    supabase.auth.getUser().then(({ data })=>{
+    const checkUser = async () => {
+
+      const { data } = await supabase.auth.getUser();
+
+      setUser(data.user);
+      setLoading(false);
 
       if(!data.user){
         router.push("/login");
       }
 
-    });
+    };
+
+    checkUser();
+
+    const { data: listener } =
+      supabase.auth.onAuthStateChange((event,session)=>{
+
+        if(!session){
+          router.push("/login");
+        }else{
+          setUser(session.user);
+        }
+
+      });
+
+    return ()=>{
+      listener.subscription.unsubscribe();
+    };
 
   },[]);
+
+
+  if(loading){
+    return(
+      <div className="loginScreen">
+        <div className="glass loginCard">
+          Завантаження...
+        </div>
+      </div>
+    );
+  }
+
 
   return(
 
@@ -30,7 +67,7 @@ export default function Home(){
 
         <h2>BUTBIT</h2>
 
-        <p>BUTBIT</p>
+        <p>{user?.email}</p>
 
       </div>
 
@@ -38,5 +75,4 @@ export default function Home(){
 
   );
 
-}
-
+    }
