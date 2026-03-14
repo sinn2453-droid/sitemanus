69857c2c9c91458e0faf17e723b92e03e58b9496
sinn2453-droid <sinn2453@gmail.com>
2026-03-14 19:36:16 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,15 @@
 import "./globals.css";
-import Sidebar from "./components/Sidebar"; // Переконайтеся, що шлях саме "./components/Sidebar"
+import Navbar from "./components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
-      <head>
-        {/* Підключення іконок для меню */}
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-      </head>
       <body>
-        <Sidebar />
+
         {children}
+
+        <Navbar />
+
       </body>
     </html>
   );
